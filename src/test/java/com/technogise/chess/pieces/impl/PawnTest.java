d6dbcac08c629e0ae4f12bdb3172e1832c917ca0
aishwary-03 <aishwary987654321@gmail.com>
2024-08-03 21:24:07 +0530

    Adds tests to PawnTest to validate a possible spot | aishwary

--- src/test/java/com/technogise/chess/pieces/impl/PawnTest.java
+++ src/test/java/com/technogise/chess/pieces/impl/PawnTest.java
@@ -1,6 +1,7 @@
 package com.technogise.chess.pieces.impl;
 
 import com.technogise.chess.domain.Spot;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,13 +10,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
+    private Pawn pawn;
+
+    @BeforeEach
+    public void setUp() {
+        pawn = new Pawn();
+    }
+
     @Test
     public void shouldReturnOnlyOneSpotWhenThePawnCanMoveForward() {
-        Pawn pawn = new Pawn();
         Spot inputSpot = new Spot('G', 1);
+
         List<Spot> possibleSpots = pawn.getAllPossibleSpots(inputSpot);
 
         assertEquals(1, possibleSpots.size());
     }
 
+    @Test
+    public void shouldReturnNoSpotsWhenThePawnCannotMoveForward() {
+        Spot inputSpot = new Spot('G', 8);
+
+        List<Spot> possibleSpots = pawn.getAllPossibleSpots(inputSpot);
+
+        assertEquals(0, possibleSpots.size());
+    }
+
+    @Test
+    public void shouldReturnTheNextRowWhenThePawnCanMoveForward() {
+        Spot inputSpot = new Spot('G', 1);
+
+        List<Spot> possibleSpots = pawn.getAllPossibleSpots(inputSpot);
+
+        Spot spot = possibleSpots.getFirst();
+        assertEquals(2, spot.row());
+    }
+
+    @Test
+    public void shouldReturnTheSameColumnWhenThePawnCanMoveForward() {
+        Spot inputSpot = new Spot('G', 1);
+
+        List<Spot> possibleSpots = pawn.getAllPossibleSpots(inputSpot);
+
+        Spot spot = possibleSpots.getFirst();
+        assertEquals('G', spot.column());
+    }
 }
