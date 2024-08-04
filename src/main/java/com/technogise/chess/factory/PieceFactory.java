1798521033e95c45349876d56e99b5d476c3565a
aishwary-03 <aishwary987654321@gmail.com>
2024-08-04 18:14:38 +0530

    Adds PieceFactory to provide piece object | aishwary

--- src/main/java/com/technogise/chess/factory/PieceFactory.java
+++ src/main/java/com/technogise/chess/factory/PieceFactory.java
@@ -1,15 +1,20 @@
 package com.technogise.chess.factory;
 
-import com.technogise.chess.domain.Piece;
+import com.technogise.chess.piece.Piece;
+import com.technogise.chess.piece.impl.King;
+import com.technogise.chess.piece.impl.Pawn;
+import com.technogise.chess.piece.impl.Queen;
 
 public class PieceFactory {
 
     public static Piece getPiece(String piece) {
         return switch (piece) {
-            case "PAWN" -> null;
-            case "KING" -> null;
-            case "QUEEN" -> null;
-            default -> throw new IllegalArgumentException();
+            case "PAWN" -> new Pawn();
+            case "KING" -> new King();
+            case "QUEEN" -> new Queen();
+            default -> {
+                throw new IllegalArgumentException("Invalid piece: " + piece);
+            }
         };
     }
 
